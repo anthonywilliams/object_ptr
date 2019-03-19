233c9d840e70fd35122b1bbf302a7142ebf34c1d
Anthony Williams <anthony@justsoftwaresolutions.co.uk>
2019-03-19 16:46:11 +0000

    Construct from nullptr

--- access_ptr.hpp
+++ access_ptr.hpp
@@ -1,10 +1,12 @@
 #ifndef JSS_ACCESS_PTR_HPP
 #define JSS_ACCESS_PTR_HPP
+#include <cstddef>
 
 namespace jss {
     template <typename T> class access_ptr {
     public:
         constexpr access_ptr() noexcept : ptr(nullptr) {}
+        constexpr access_ptr(std::nullptr_t) noexcept : ptr(nullptr) {}
 
         constexpr T *get() const noexcept {
             return ptr;
--- test_access_ptr.cpp
+++ test_access_ptr.cpp
@@ -1,12 +1,22 @@
-#include <assert.h>
 #include "access_ptr.hpp"
+#include <assert.h>
 
 void test_access_ptr_default_constructs_to_null() {
     jss::access_ptr<int> ap;
-
     assert(ap.get() == nullptr);
+}
+
+void test_access_ptr_can_be_constructed_from_nullptr() {
+    jss::access_ptr<int> ap(nullptr);
+    assert(ap.get() == nullptr);
+
+    jss::access_ptr<int> ap2=nullptr;
+    assert(ap2.get() == nullptr);
+    
+    [](jss::access_ptr<int> ap3) { assert(ap3.get() == nullptr); }(nullptr);
 }
 
 int main() {
     test_access_ptr_default_constructs_to_null();
+    test_access_ptr_can_be_constructed_from_nullptr();
 }
